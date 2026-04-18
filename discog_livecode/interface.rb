7fe7a98c8a0de9311a4b31b7b484aa4aadf9602a
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2026-04-18 18:34:15 +0900

    wrote interface

--- discog_livecode/interface.rb
+++ discog_livecode/interface.rb
@@ -1,2 +1,23 @@
 require_relative 'scraper'
+require 'json'
 # TODO: scrape the discog page, generate JSON, save them into a file.
+
+puts 'fetching top 10 URLs'
+urls = fetch_master_urls
+
+# Prepare a place to store hashes
+data = []
+
+puts 'Getting more detailed info'
+
+urls.each do |url|
+  puts "fetching #{url}"
+  # psh the fetched data of an album
+  data << scrape_album(url)
+end
+
+File.open('data/best_records.json', 'wb') do |file|
+  file.write(JSON.pretty_generate(data))
+end
+
+puts 'Done!'
