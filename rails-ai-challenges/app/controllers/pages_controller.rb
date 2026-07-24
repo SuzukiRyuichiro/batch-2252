d4981334f8d07e2a137f503b8dd8dce24627a35e
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2026-07-24 18:22:54 +0900

    ready for lec

--- rails-ai-challenges/app/controllers/pages_controller.rb
+++ rails-ai-challenges/app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :demo_message]
 
   def home
+  end
+
+  def demo_message
+    @text = params[:text]
   end
 end
