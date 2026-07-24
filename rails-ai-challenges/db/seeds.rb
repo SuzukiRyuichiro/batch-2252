7f4877f09c7efeb0ed2c17626bd3835d87773222
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2026-07-24 18:05:44 +0900

    add a simple link

--- rails-ai-challenges/db/seeds.rb
+++ rails-ai-challenges/db/seeds.rb
@@ -3,6 +3,12 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 
+Chat.destroy_all
+Challenge.destroy_all
+User.destroy_all
+Message.destroy_all
+
+User.create(email: 'mail@mail.com', password: '123123')
 
 Challenge.create(
   name: "HTML Generator",
